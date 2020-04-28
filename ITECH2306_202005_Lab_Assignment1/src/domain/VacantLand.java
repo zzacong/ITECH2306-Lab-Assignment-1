d754ed2cd5439a0134962eaaef4b305b12380b6d
Sailendrakc <466669999+Sailendrakc@users.noreply.github.com>
2020-04-29 07:56:23 +1000

    Implemented for VacantLand type

--- ITECH2306_202005_Lab_Assignment1/src/domain/VacantLand.java
+++ ITECH2306_202005_Lab_Assignment1/src/domain/VacantLand.java
@@ -2,30 +2,68 @@ package domain;
 
 /**
  * @author Takeogh
- *
+ * @author Arun
  */
 public class VacantLand extends Property{
 
+	private static final double CIV_RATE = 0.002;
+	private static final double FIRE_SERVICES_BASE = 110;
+	private static final double FIRE_SERVICES_PERCENT = 0.00006;
+	private ServiceType fireServicesLevy;
 	private String[] overlays;
 
 	public VacantLand() {
-		System.out.println("Not implemented yet");
+		super();
+		setCapitalImprovedRate(CIV_RATE);
 	}
+	
 	public String[] getOverlays() {
 		return overlays;
 	}
+	
 	public void setOverlays(String[] overlays) {
 		this.overlays = overlays;
 	}
+	
 	@Override
 	public double calculateExtraServices() {
-		// TODO Auto-generated method stub
-		return 0;
+
+		return fireServicesLevy.calculateChargeForServiceType();
 	}
+	
 	@Override
 	public void setUpExtraServices() {
-		// TODO Auto-generated method stub
-		
+
+		fireServicesLevy = new BaseAndPercentageOfValueService("Fire Levy",
+				FIRE_SERVICES_BASE,
+				FIRE_SERVICES_PERCENT,
+				getCapitalImprovedValue());
+	}
+	
+	public ServiceType getFireServicesLevy() {
+		return fireServicesLevy;
+	}
+	
+	public void setFireServicesLevy(ServiceType fireServicesLevy) {
+		this.fireServicesLevy = fireServicesLevy;
+	}
+	
+	public static double getCivRate() {
+		return CIV_RATE;
+	}
+	
+	@Override
+	public String toString() {
+		return super.toString() + "OtherProperty [\n" +
+								  fireServicesLevy.toString() + "]\n ";	
+	}
+	
+	public static double getFireServicesBase() {
+		return FIRE_SERVICES_BASE;
+	}
+	
+	public static double getFireServicesPercent() {
+		return FIRE_SERVICES_PERCENT;
 	}
 
 }
