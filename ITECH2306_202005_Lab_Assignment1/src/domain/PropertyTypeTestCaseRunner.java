92d371f8bf04a1409cc7da02df1b96f783faebb8
anushpoudel <apoudel55@gmail.com>
2020-04-30 19:40:48 +1000

    V4.9 by Anush

--- ITECH2306_202005_Lab_Assignment1/src/domain/PropertyTypeTestCaseRunner.java
+++ ITECH2306_202005_Lab_Assignment1/src/domain/PropertyTypeTestCaseRunner.java
@@ -4,17 +4,27 @@
 package domain;
 
 import org.junit.runner.JUnitCore;
+import java.util.ArrayList;
 import org.junit.runner.Result;
 
 /**
  * @author Zac
+ * @author Anush
  *
  */
 public class PropertyTypeTestCaseRunner {
 
 	public static void main(String[] args) {
+		
 		Result result = JUnitCore.runClasses(SchoolCommunityTestCase.class, CommercialTestCase.class);
-		System.out.println(result.getFailures());
+		ArrayList<String> emptyList = new ArrayList<String>();
+		
+		if (result.getFailures().equals(emptyList)) {
+			System.out.println("All tests ran successfully.");			
+		}
+		else {
+			System.out.println(result.getFailures());
+		}
 	}
 
 }
