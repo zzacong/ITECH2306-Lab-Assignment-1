dde84858ee2668dafdaf49a95ad33efa8c85888e
anushpoudel <apoudel55@gmail.com>
2020-05-21 13:28:21 +1000

    V6.15 by Anush

--- ITECH2306_202005_Lab_Assignment1/src/domain/Commercial.java
+++ ITECH2306_202005_Lab_Assignment1/src/domain/Commercial.java
@@ -5,6 +5,9 @@ import utility.Validator;
 /**
  * @author Anush
  * 
+ * @version 6.14. Concrete class of abstract class Property. 
+ * We validate all attributes of the property and will throw an exception for the invalid inputs. 
+ * Waste Management and Fire Services Levy are charged together with CIV value multiplied by a CIV rate. 
  */
 public class Commercial extends Property {
 	private String businessName;
@@ -17,6 +20,25 @@ public class Commercial extends Property {
 	private ServiceType wasteManagement;
 	private ServiceType fireServicesLevy;
 
+	/**
+	 * Construct a new Commercial Property which inherits all attributes of Property and Commercial class. 
+	 * Instantiate the Property class with its nine-parameter constructor by passing in the Property class attributes. 
+	 * Set the Commercial class attributes based on the values passed in from the parameters. 
+	 * It will throw an exception if any attribute passed in is null or empty.
+	 * 
+	 * @param description A string to define the description of property
+	 * @param location A string to define the location of property
+	 * @param area A double to specify the area of property in square meters
+	 * @param siteValue A double to specify the site value of property
+	 * @param capitalImprovedValue A double to specify the Capital Improved Value of property, it should be greater than the site value
+	 * @param capitalImprovedRate A double to specify the Capital Improved Rate of property
+	 * @param netAnnualValue A double to specify the net annual value of property
+	 * @param valuationDate A string to define the valuation date of property
+	 * @param owner A RatePayer object to specify the owner of property
+	 * @param businessName A string to define the name of the Commercial property
+	 * @param abn A string to define the Australian Business Number(ABN) of the Commercial property
+	 * @throws NullPointerException if any parameter passed in is null or empty
+	 */
 	public Commercial(String description, String location, double area, double siteValue, double capitalImprovedValue, double capitalImprovedRate, 
 			   double netAnnualValue, String valuationDate, RatePayer owner, String businessName, String abn) throws NullPointerException {
 		super(description, location, area, siteValue, capitalImprovedValue, capitalImprovedRate, netAnnualValue, valuationDate, owner);
@@ -24,6 +46,12 @@ public class Commercial extends Property {
 		this.setAbn(abn);
 	}
 	
+	/**
+	 * Construct a new Commercial property with zero parameter.
+	 * Instantiate the Property class with its zero-parameter constructor. 
+	 * Assign default values for Commercial class attributes. 
+	 * Set the Capital Improved Rate on the Property class for this property type.
+	 */
 	public Commercial() {
 		super();
 		// Explicitly assign defaults for String
@@ -32,10 +60,17 @@ public class Commercial extends Property {
 		setCapitalImprovedRate(CIV_RATE);
 	}
 
+	/**
+	 * @return the business name of the property
+	 */
 	public String getBusinessName() {
 		return businessName;
 	}
 	
+	/**
+	 * @param businessName assigns the passed in string as the businessName of this Commercial property  
+	 * @throws NullPointerException if the businessName is null or empty
+	 */
 	public void setBusinessName(String businessName) throws NullPointerException {
 		if (Validator.validateString("Business Name", businessName)) {
 			this.businessName = businessName;
@@ -44,11 +79,18 @@ public class Commercial extends Property {
 			throw new NullPointerException("Business name for Commercial property is null or empty. Rejecting this record...\n");
 		}
 	}
-
+	
+	/**
+	 * @return the ABN of the property
+	 */
 	public String getAbn() {
 		return abn;
 	}
 
+	/**
+	 * @param abn assigns the passed in string as the abn of this Commercial property  
+	 * @throws NullPointerException if the abn is null or empty
+	 */
 	public void setAbn(String abn) throws NullPointerException {
 		if (Validator.validateString("ABN", abn)) {
 			this.abn = abn;
@@ -57,8 +99,10 @@ public class Commercial extends Property {
 			throw new NullPointerException("ABN for Commercial Property null or empty. Rejecting this record...\n");
 		}
 	}
-
-	// Set up the extra services of Commercial property type
+	
+	/**
+	 * Overridden method to setup the extra services on the Commercial property. For Commercial property, Waste Management and Fire Services Levy fees are added.
+	 */
 	@Override
 	public void setUpExtraServices() {
 		setHasExtraServices(true);
@@ -71,13 +115,19 @@ public class Commercial extends Property {
 																getCapitalImprovedValue());		
 	}
 
-	// Add up all the extra services charges
+	/**
+	 * Overridden method to calculate the extra services fees by adding them together.
+	 * @return the extra services fees of this commercial property
+	 */
 	@Override
 	public double calculateExtraServices() {
 		return wasteManagement.calculateChargeForServiceType() +
 				   fireServicesLevy.calculateChargeForServiceType();
 	}
 	
+	/**
+	 * @return a string representing the toString() of the extra services for Waste Management and Fire Services Levy if extra services are already set up
+	 */
 	public String extraServices() {
 		if (getHasExtraServices()) {
 			return  "Property extra services: [\n" + wasteManagement.toString() + "\n" + 
@@ -88,6 +138,9 @@ public class Commercial extends Property {
 		}
 	}
 	
+	/**
+	 * @return a string showing all the attributes on the property; and the details of extra services if there's any  
+	 */	
 	@Override 
 	public String toString() {
 		return  super.toString() + "Property type: Commercial [" + 
